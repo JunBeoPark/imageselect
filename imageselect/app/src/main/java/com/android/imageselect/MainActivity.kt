8ee2e4db357db2555ac9cbfa233e6c919e80d2ac
Jun Beom Park <94429684+JunBeoPark@users.noreply.github.com>
2023-09-21 11:22:44 +0900

    Update MainActivity.kt

--- imageselect/app/src/main/java/com/android/imageselect/MainActivity.kt
+++ imageselect/app/src/main/java/com/android/imageselect/MainActivity.kt
@@ -1,11 +1,49 @@
-package com.android.imageselect
+package com.android.selectimage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    var liked: ArrayList<SearchItemModel> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.run {
+            btnSearchFragment.setOnClickListener{
+                setFragment(SearchFragment())
+            }
+            btnBookmarkFragment.setOnClickListener {
+                setFragment(BookmarkFragment())
+            }
+        }
+
+        setFragment(Searchfragment())
+    }
+
+    private fun setFragment(frag : Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.frameLayout, frag)
+            setReorderingAllowed(true)
+            addToBackStack(null)
+        }
+    }
+
+    fun addLikedItem(item: SearchItemModel) {
+        if(!likedItems.contains(item)) {
+            likedItems.add(item)
+        }
+    }
+
+    fun removeLikedItem(item: SearchItemModel) {
+        likedItems.remove(item)
     }
 }
